bb5323b953893f950df0e89f83020d923f714744
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-08-28 19:12:43 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TftAuthApp::Application.config.secret_token = '1328027a70f2064cc46f4c04a7bdd9b47e97894626aa148d6b3f22bc0a1d255fb481b752bf7066d86f1933c1bdd32b07e90a91163246126ccb8c3a9798b7d701'
