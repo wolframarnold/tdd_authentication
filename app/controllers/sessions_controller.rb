88e34fad884b48c60786251542b2c27b7d6ebd76
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-08-30 15:48:48 -0700

    Display name and image on layout Conditional sign in/sign out link No tests, sign out link is placeholder

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
 
   def create
-    raise request.env['omniauth.auth'].to_yaml  # Do this in Firefox (not Chrome) -- it looks better
+    flash[:notice] = "Successfully logged in"
+    session[:name] = request.env['omniauth.auth']['user_info']['name']
+    session[:uid] = request.env['omniauth.auth']['uid']
+    session[:provider] = request.env['omniauth.auth']['provider']
+    session[:avatar] = request.env['omniauth.auth']['user_info']['image']
+    session[:nickname] = request.env['omniauth.auth']['user_info']['nickname']
+    redirect_to root_path
   end
 
   def failure
-    render :text => "OOPS"
+    flash[:alert] = "Authentication failure"
+    redirect_to root_path
   end
 
 end
