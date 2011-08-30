fc9038a9e77ea7f654ed9b3361596f5b5305740c
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-08-29 22:48:35 -0700

    remove secrets, and add stubs for Facebook and LinkedIn as well

--- config/initializers/omniauth_sample.rb
+++ config/initializers/omniauth_sample.rb
@@ -1,5 +1,10 @@
 # Note: This MUST BE REMOVED if you're using Devise's omniauthable, or Devise will report "invalid credentials" when attempting to log in via Twitter
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'RaRwrok5RQitDkbh01xcnQ', 'yTcovj1k1Lb3XQ4mRyOjIBlApKoQaDCJQCXWV28Sw' # order: Consumer Key, Consumer Secret
+  provider :twitter, 'Consumer Key', 'Consumer Secret' # order: Consumer Key, Consumer Secret
   # option: :force_login => true will force a new login into the Provider, even though the Provider may have previously been logged in, from a different browser tab.
+
+  provider :facebook, 'APP ID/API Key', 'App Secret'
+
+  provider :linked_in, 'API Key', 'Secret Key'
 end
